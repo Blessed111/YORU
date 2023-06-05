afa48515e74b42f3873fb582551c2179206e79ee
Imangali <ospan202022@gmail.com>
2023-06-05 22:22:55 +0600

    baha

--- pkg/cfg/session.go
+++ pkg/cfg/session.go
@@ -9,7 +9,8 @@ const SESSION_ID = "user_logged_token"
 var Store = sessions.NewCookieStore([]byte("ajksgk0934712qwfqqr"))
 
 func init() {
-	Store.Options.HttpOnly = true
-	Store.Options.Secure = true
+	Store.Options.HttpOnly = false
+	Store.Options.Secure = false
 	Store.Options.MaxAge = 3600 * 24
+	
 }
